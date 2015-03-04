95e4258ff516086ae5ba31d09577026ad17af526
Skidlish <jesper.svennebring@gmail.com>
2015-03-04 21:12:57 +0100

    Revert "Merging templates and removing the copy"
    This reverts commit 1bffb8fd1a786a467e5cc005d3e3dd4dfca86426.

--- jscripts.js
+++ jscripts.js
@@ -407,11 +407,9 @@ function send_request(data) {
     document.getElementById("status").innerHTML = "processing..";
 }
 
-
 function setLanguage (inputLang) {
     $(function() {
         var language = inputLang;
-        var $saveLanguage = localStorage.setItem("currentLang", inputLang);
         $.ajax({
             url: 'languages.xml',
             success: function(xml) {
@@ -426,11 +424,7 @@ function setLanguage (inputLang) {
 
 }
 
-
 $(document).ready(function(){
-    if(localStorage["currentLang"] == null){ localStorage.setItem("currentLang", "english") }
-    setLanguage(localStorage.getItem("currentLang"));
-
     $("img.lang").on("click", function () {
 	if($(this).hasClass("sv")){
             $("#swedish").addClass("hidden");
